b3a9e410e5b74919c8c095b92ddbdac6781b4513
N N Advith <nnadvith08042001@gmail.com>
2025-03-17 19:11:59 +0530

    tests; unit and functional - inprogress

---
+++ mantis_linux_unit_test.go
@@ -0,0 +1,20 @@
+//go:build linux
+
+package main_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	mantis "github.com/nn-advith/mantis"
+)
+
+func TestGetGlobalConfigPath(t *testing.T) {
+	t.Setenv("HOME", "/home/testusr")
+	expected := filepath.Join("/home/testusr", ".config", "mantis", "mantis.json")
+	result := mantis.GetGlobalConfigPath()
+
+	if expected != result {
+		t.Errorf("Expected %s; Got %s", expected, result)
+	}
+}
