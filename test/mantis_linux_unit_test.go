360afd192cfc8b45787854c988dab05587fc28f0
N N Advith <nnadvith08042001@gmail.com>
2025-03-23 02:17:11 +0530

    unit tests and refactor

--- test/mantis_linux_unit_test.go
+++ test/mantis_linux_unit_test.go
@@ -4,9 +4,12 @@ package main_test
 
 import (
 	"path/filepath"
+	"regexp"
+	"syscall"
 	"testing"
 
 	"github.com/nn-advith/mantis/mantis"
+	"github.com/nn-advith/mantis/testutils"
 )
 
 func TestGetGlobalConfigPath(t *testing.T) {
@@ -16,5 +19,60 @@ func TestGetGlobalConfigPath(t *testing.T) {
 
 	if expected != result {
 		t.Errorf("Expected %s; Got %s", expected, result)
+	}
+}
+
+func TestCommandConstruct(t *testing.T) {
+	globalargs := map[string][]string{
+		"-f": {"testdata/sample.go"},
+		"-a": {""},
+		"-e": {},
+	}
+	mantis.SetGlobalArgs(globalargs)
+
+	cmd, err := mantis.CommandConstruct()
+	if err != nil {
+		t.Fatalf("failed command construct, %v", err)
+	}
+	expectedPath := `^.*\/go$`
+	re := regexp.MustCompile(expectedPath)
+	expectedArgs := []string{"go", "run", "testdata/sample.go", ""}
+
+	var sliceCompare = func(a, b []string) bool {
+		if len(a) != len(b) {
+			return false
+		}
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+		return true
+	}
+
+	if !re.MatchString(cmd.Path) {
+		t.Errorf("go binary not present in path")
+	}
+	if !sliceCompare(expectedArgs, cmd.Args) {
+		t.Errorf("expected args for cmd %v; got %v", expectedArgs, cmd.Args)
+	}
+}
+
+func TestKillProcess(t *testing.T) {
+
+	cmd := testutils.CreateDummyProcess(t)
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	if err := cmd.Start(); err != nil {
+		t.Fatalf("error starting dummy process")
+	}
+
+	mantis.SetCProcess(cmd.Process)
+	mantis.SuppressLog(1)
+	mantis.KillProcess()
+	mantis.SuppressLog(0)
+	if mantis.GetCProcess() != nil {
+		t.Errorf("failed; expected cprocess to be nil")
+	} else {
 	}
 }
--- test/mantis_windows_unit_test.go
+++ test/mantis_windows_unit_test.go
@@ -6,9 +6,11 @@ package main_test
 
 import (
 	"path/filepath"
+	"regexp"
 	"testing"
 
 	"github.com/nn-advith/mantis/mantis"
+	"github.com/nn-advith/mantis/testutils"
 )
 
 func TestGetGlobalConfigPath(t *testing.T) {
@@ -20,4 +22,59 @@ func TestGetGlobalConfigPath(t *testing.T) {
 	if result != expected {
 		t.Errorf("Expected %s; Got %s", expected, result)
 	}
+}
+
+func TestCommandConstruct(t *testing.T) {
+	globalargs := map[string][]string{
+		"-f": {"testdata/sample.go"},
+		"-a": {""},
+		"-e": {},
+	}
+	mantis.SetGlobalArgs(globalargs)
+
+	cmd, err := mantis.CommandConstruct()
+	if err != nil {
+		t.Fatalf("failed command construct, %v", err)
+	}
+	expectedPath := `^.*go\.exe$`
+	re := regexp.MustCompile(expectedPath)
+	expectedArgs := []string{"go", "run", "testdata/sample.go", ""}
+
+	var sliceCompare = func(a, b []string) bool {
+		if len(a) != len(b) {
+			return false
+		}
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+		return true
+	}
+
+	if !re.MatchString(cmd.Path) {
+		t.Errorf("go binary not present in path")
+	}
+	if !sliceCompare(expectedArgs, cmd.Args) {
+		t.Errorf("expected args for cmd %v; got %v", expectedArgs, cmd.Args)
+	}
+
+}
+
+func TestKillProcess(t *testing.T) {
+
+	cmd := testutils.CreateDummyProcess(t)
+
+	if err := cmd.Start(); err != nil {
+		t.Errorf("error starting dummy process")
+	}
+
+	mantis.SetCProcess(cmd.Process)
+	mantis.SuppressLog(1)
+	mantis.KillProcess()
+	mantis.SuppressLog(0)
+	if mantis.GetCProcess() != nil {
+		t.Errorf("failed; expected cprocess to be nil")
+	}
+
 }
